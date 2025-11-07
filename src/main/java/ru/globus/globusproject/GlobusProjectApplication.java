46af94597162010d6c22589517e3ed858e16a40b
vilar <loadslowly@gmail.com>
2025-11-07 23:28:48 +0500

    init: initial commit of the project

---
+++ src/main/java/ru/globus/globusproject/GlobusProjectApplication.java
@@ -0,0 +1,14 @@
+package ru.globus.globusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobusProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobusProjectApplication.class, args);
+        Object
+    }
+
+}
---
+++ src/test/java/ru/globus/globusproject/GlobusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.globus.globusproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobusProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
